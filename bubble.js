95be41fab7e0f2f478deca7eaa1f7f7e36569dc3
Eric Moore <ericmoore0709@gmail.com>
2024-10-17 23:44:27 -0700

    Completed requred study

--- bubble.js
+++ bubble.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return arr;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,45 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let sortedArr = [];
 
-function mergeSort() {}
+    let ptr1 = 0;
+    let ptr2 = 0;
 
-module.exports = { merge, mergeSort};
+    while (ptr1 < arr1.length && ptr2 < arr2.length) {
+        if (arr1[ptr1] < arr2[ptr2]) {
+            sortedArr.push(arr1[ptr1]);
+            ptr1++;
+        } else {
+            sortedArr.push(arr2[ptr2]);
+            ptr2++;
+        }
+    }
+
+    while (ptr1 < arr1.length) {
+        sortedArr.push(arr1[ptr1]);
+        ptr1++;
+    }
+
+    while (ptr2 < arr2.length) {
+        sortedArr.push(arr2[ptr2]);
+        ptr2++;
+    }
+
+    return sortedArr;
+}
+
+function mergeSort(arr) {
+
+    if (arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length / 2);
+    const firstArr = arr.slice(0, mid);
+    const secondArr = arr.slice(mid);
+
+    const sortedFirst = mergeSort(firstArr);
+    const sortedSecond = mergeSort(secondArr);
+
+    return merge(sortedFirst, sortedSecond);
+
+}
+
+module.exports = { merge, mergeSort };
